61fd97260686b20f89a3d3fca535801c313253d1
AbouYeo <kagnekoyeo@gmail.com>
2025-08-12 15:46:47 +0200

    Optimisation de l´affichage du tableau

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -1,35 +1,26 @@
-type User = {
-    id: string;
-    firstName: string;
-    lastName: string;
-    phone?: string;
-    email?: string;
-    company?: string;
-    address?: Address;
-};
-
-type Address = {
-    street: string;
-    houseNumber: number;
-    city: string;
-    zipcode: number;
-};
+import type { User } from "../utils/types";
 
 function UserTable(user: User) {
     return (
-        <tr className="bg-fuchsia-300 border-3 ">
-            <td className="border-3 md:p-2">{user.firstName}</td>
-            <td className="border-3 md:p-2">{user.lastName}</td>
-            <td className="border-3 md:p-2">
-                {user.phone ? <>{user.phone}</> : null}
-            </td>
-            <td className="border-3 md:p-2">
-                {user.email ? <>{user.email}</> : null}
-            </td>
-            <td className="border-3 md:p-2">
-                {user.company ? <>{user.company}</> : null}
-            </td>
-        </tr>
+        <>
+            <tr className="bg-fuchsia-300 border-2  ">
+                <td className="border-2 w-fit p-2 md:px-5 md:text-2xl">
+                    {user.firstName}
+                </td>
+                <td className="border-2 w-fit p-2 md:px-5 md:text-2xl">
+                    {user.lastName}
+                </td>
+                <td className="border-2 w-fit p-2 md:px-5 md:text-2xl">
+                    {user.phone ? <>{user.phone}</> : null}
+                </td>
+                <td className="border-2 w-fit p-2 md:px-5 md:text-2xl">
+                    {user.email ? <>{user.email}</> : null}
+                </td>
+                <td className="border-2 w-fit p-2 md:px-5 md:text-2xl">
+                    {user.company ? <>{user.company}</> : null}
+                </td>
+            </tr>
+        </>
     );
 }
 
--- src/pages/users/users_as_table/UsersAsTable.tsx
+++ src/pages/users/users_as_table/UsersAsTable.tsx
@@ -6,17 +6,44 @@ type Props = {
 };
 function UsersAsTable(props: Props) {
     const users = props.users;
-    return users.map((user: User) => (
-        <UserTable
-            firstName={user.firstName}
-            lastName={user.lastName}
-            id={user.id}
-            email={user.email}
-            company={user.company}
-            address={user.address}
-            phone={user.phone}
-        />
-    ));
+    return (
+        <table>
+            <thead>
+                <tr className="bg-fuchsia-300 border-2">
+                    <th className="border-2 w-fit p-2 md:px-5 md:text-2xl">
+                        Prénom
+                    </th>
+                    <th className="border-2 w-fit p-2 md:px-5 md:text-2xl">
+                        Nom
+                    </th>
+                    <th className="border-2 w-fit p-2 md:px-5 md:text-2xl">
+                        Telephone
+                    </th>
+                    <th className="border-2 w-fit p-2 md:px-5 md:text-2xl">
+                        Email
+                    </th>
+                    <th className="border-2 w-fit p-2 md:px-5 md:text-2xl">
+                        Employeur
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map((user: User) => (
+                    <UserTable
+                        index={0}
+                        key={user.id}
+                        firstName={user.firstName}
+                        lastName={user.lastName}
+                        id={user.id}
+                        email={user.email}
+                        company={user.company}
+                        address={user.address}
+                        phone={user.phone}
+                    />
+                ))}
+            </tbody>
+        </table>
+    );
 }
 
 export default UsersAsTable;
