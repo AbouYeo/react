8724b284fd03917e7dcb3e8b6e12e99da5c29607
AbouYeo <kagnekoyeo@gmail.com>
2025-08-23 09:13:14 +0200

    Optimisation du design

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,36 @@ import "./App.css";
 
 function App() {
     return (
-        <section className="md:px-20  px-4 container mx-auto">
-            <div>
-                <Link to="/">LES NOUVELLES VIES</Link>
-                <nav>
-                    <Link to="/login"> Se connecter</Link>
-                    <Link to="/about"> About</Link>
-                    <Link to="/users"> Users</Link>
+        <section className="md:px-20  px-4 container mx-auto mt-2">
+            <div className="flex justify-between items-center">
+                <Link
+                    className=" border rounded py-1.5 px-3 shadow-2xl text-2xl"
+                    to="/"
+                >
+                    LES NOUVELLES VIES
+                </Link>
+                <nav className="flex justify-between gap-4">
+                    <Link
+                        className="hover:bg-zinc-500 px-4 py-1.5 rounded"
+                        to="/login"
+                    >
+                        {" "}
+                        Se connecter
+                    </Link>
+                    <Link
+                        className="hover:bg-zinc-500 px-4 py-1.5 rounded"
+                        to="/about"
+                    >
+                        {" "}
+                        About
+                    </Link>
+                    <Link
+                        className="hover:bg-zinc-500 px-4 py-1.5 rounded"
+                        to="/users"
+                    >
+                        {" "}
+                        Users
+                    </Link>
                 </nav>
             </div>
 
--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,10 +1,12 @@
 function HomePage() {
     return (
-        <div className="flex flex-row gap-3 w-full bg-amber-400 h-96 p-2">
-            <button className=" bg-blue-900 px-3 rounded-3xl text-emerald-50">
+        <div className="flex flex-row gap-3 w-full my-3  h-96 p-2">
+            <button className=" bg-green-700 hover:bg-green-800 px-3 shadow-xl rounded-3xl font-bold text-emerald-50 w-4xl">
                 Enregistrer une nouvelle naissance
             </button>
-            <button className=" ">Traiter les naissances</button>
+            <button className="bg-sky-100 hover:bg-sky-200 px-3 rounded-3xl shadow-xl text-black font-bold w-4xl">
+                Traiter les naissances
+            </button>
         </div>
     );
 }
